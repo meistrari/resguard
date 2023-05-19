0831077de380606594acd5ec48f0c6ba7c47c143
henrycunh <henrycunh@gmail.com>
2023-05-18 23:47:56 -0300

    fix: correct eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 class TryResult<T extends any | null, E extends Error | null> {
     private [Symbol.iterator] = function* (): IterableIterator<[T | null, E | null]> {
-        yield [this.data, this.error];    
+        yield [this.data, this.error]
     }
-    public data: T | null;
-    public error: Error | null;
+
+    public data: T | null
+    public error: Error | null
 
     constructor(data: T, error: E | null) {
-        this.data = data;
-        this.error = error;
+        this.data = data
+        this.error = error
     }
 }
 
 export async function resguard<T, E extends ErrorClass>(
-    promise: Promise<T> | (() => T), 
-    _errorType?: E
-): Promise<TryResult<T | null, InstanceType<E> | null>>  {
+    promise: Promise<T> | (() => T),
+    _errorType?: E,
+): Promise<TryResult<T | null, InstanceType<E> | null>> {
     try {
-        if (typeof promise === 'function') {
+        if (typeof promise === 'function')
             return new TryResult(promise(), null)
-        }
-        else {
+
+        else
             return new TryResult(await promise, null)
-        }
     }
     catch (e: any) {
         return new TryResult(null, e as InstanceType<E>)
@@ -29,4 +29,3 @@ export async function resguard<T, E extends ErrorClass>(
 }
 
 type ErrorClass = new (...args: any[]) => Error
-
